0e47e3462b2da4cc19df14f75b07a1721e2bdd26
Aribo78 <89050884+Aribo78@users.noreply.github.com>
2023-02-14 21:19:07 -0500

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,10 +1,11 @@
-
-function addUser() {
-
-    user_name = document.getElementById("user_name").value;
-  
-    localStorage.setItem("user_name", user_name);
-    
-      window.location = "Kwitter_room.html";
-  }
-  
+var firebaseConfig = {
+    apiKey: "AIzaSyDh8u_aQ30QCTI-HAxHXa5cyt4R8xUCC4E",
+    authDomain: "let-chat-web-app-b6a4b.firebaseapp.com",
+    databaseURL: "https://let-chat-web-app-b6a4b-default-rtdb.firebaseio.com",
+    projectId: "let-chat-web-app-b6a4b",
+    storageBucket: "let-chat-web-app-b6a4b.appspot.com",
+    messagingSenderId: "380645790987",
+    appId: "1:380645790987:web:38be5cb397be516e82fe0e",
+    measurementId: "G-4RDPDV2F55"
+  };
+  firebase = initializeApp(firebaseConfig);
